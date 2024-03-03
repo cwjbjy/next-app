8b73321ff3781d5c6648799b17614e6fecfbb0ad
cwj <595842570@qq.com>
2024-03-03 21:32:40 +0800

    feat: 配置ts规则

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "mini.eastday.com",
+        protocol: 'https',
+        hostname: 'mini.eastday.com',
       },
       {
-        protocol: "https",
-        hostname: "dfzximg02.dftoutiao.com",
+        protocol: 'https',
+        hostname: 'dfzximg02.dftoutiao.com',
       },
     ],
   },
